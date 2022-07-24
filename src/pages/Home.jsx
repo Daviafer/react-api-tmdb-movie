83168504a66aa675d18a306597a393a6781bcbfb
Daviafer <daviafer1@gmail.com>
2022-07-24 20:12:05 -0300

    Pegando dados da API, melhorando o HOME

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,28 @@
-const Home = () => {
-  return <div>Home</div>;
-};
+import { useState, useEffect } from "react";
 
-export default Home;
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
+const Home = () => {
+  const[topMovies, setTopMovies] = useState([]);
+  
+  const getTopRateMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+
+    setTopMovies(data.results)
+  }
+  useEffect(() => {
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`
+    
+    getTopRateMovies(topRatedUrl)
+  }, [])
+
+  return ( 
+    <div>{
+      topMovies && topMovies.map((movie) => <p>{movie.title}</p>)}
+    </div>
+  )
+}
+
+export default Home
